330621e7711719a9a93111678f86af3b5dee174c
ozkandemiircan@gmail.com <ozkandemiircan@gmail.com>
2021-04-25 19:45:34 +0300

    Add project files.

---
+++ DataAcsessLayer/Abstract/IContactDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcsessLayer.Abstract
+{
+    public interface IContactDal:IRepository<Contact>
+    {
+
+    }
+}
